90634b024e263c8c5321942fe5a3617f028f3aa7
jonathans199 <jso1919@gmail.com>
2023-01-07 14:28:13 -0500

    product model, controller, creation, and query working properly

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -5,7 +5,15 @@ const ProductSchema = new mongoose.Schema(
     product_id: { type: Number, required: true },
     sku: { type: String, required: true },
     created_by: { type: Object, required: true },
-    updated_by: { type: { user_id: Number, username: String } },
+    updated_by: { type: { user_id: { type: Number, required: true }, username: { type: String, required: true } } },
+    logs: {
+      type: {
+        date: { type: Date, required: true },
+        update_type: { type: String, required: true },
+        username: { type: String, required: true },
+        previous_data: { type: { field: { type: String, required: true }, value: { type: String, required: true } } },
+      },
+    },
     images: { type: Array },
     description: { type: String, required: true },
     price: { type: String, required: true },
